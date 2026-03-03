a7ae803d6464aa7bce225a90b3aa79cba85f82c5
phaer <phaer@phaer.org>
2026-03-03 08:25:05 +0100

    feat: add devshell with nix-unit and nixfmt-tree
    Add nixpkgs input for the devshell. Replace 'use nix' with 'use flake'
    in .envrc.

--- flake.nix
+++ flake.nix
@@ -3,23 +3,32 @@
 
   inputs = {
     adios-flake.url = "github:Mic92/adios-flake";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { adios-flake, ... }:
+  outputs = { adios-flake, nixpkgs, ... }:
     let
       imported = import ./nix { inherit adios-flake; };
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      eachSystem = f: builtins.foldl' (acc: system: acc // {
+        ${system} = f (import nixpkgs { inherit system; });
+      }) {} systems;
     in
     {
       lib = imported // {
         __functor = _: args:
           imported.mkFlake (args // {
-            systems = args.systems or [
-              "x86_64-linux"
-              "aarch64-linux"
-              "aarch64-darwin"
-              "x86_64-darwin"
-            ];
+            systems = args.systems or systems;
           });
       };
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nix-unit
+            pkgs.nixfmt-tree
+          ];
+        };
+      });
     };
 }
